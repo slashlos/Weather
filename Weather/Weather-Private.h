1c22294632c975e266aca7c0e46dc655d175c05b
slashlos <slashlos@gmail.com>
2016-10-24 18:55:20 -0400

    update copyright

--- Weather/Weather-Private.h
+++ Weather/Weather-Private.h
@@ -3,7 +3,7 @@
 //  Weather
 //
 //  Created by Carlos Santiago on 8/28/16.
-//  Copyright 2016 slashlos. All rights reserved.
+//  Copyright © 2016 Carlos D. Santiago. All rights reserved.
 //
 //	Split from Extras.h
 //
